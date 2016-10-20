f59af18e6434adce548b0ffc0e09f9794719b624
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2016-10-20 13:48:06 +0700

    add rai bang wa forget law

--- public/js/footer.js
+++ public/js/footer.js
@@ -27,8 +27,8 @@ $("#checkRegisterCodeForm").submit(function (e) {
     checkRegisterCode();
 });
 function navAdapt() {
-  if($(window).scrollTop() >= 530) {
-    if($(window).scrollTop() > 730) {
+  if($(window).scrollTop() >= 470) {
+    if($(window).scrollTop() > 670) {
       $('#nav_primary').css("padding-top","1em");
       $('#nav_primary').css("background-color","rgba(0,0,0,1)");
       $('#home-nav').attr('class','hvr-underline-from-center nav-my-menu');
@@ -38,8 +38,8 @@ function navAdapt() {
       $('#schdule-nav').attr('class','hvr-underline-from-center nav-my-menu');
     }
     else {
-      var mapPaddingTop = 4-(($(window).scrollTop()-530)*1.5)/100;
-      var mapRGBA = ($(window).scrollTop()-530)/200;
+      var mapPaddingTop = 4-(($(window).scrollTop()-470)*1.5)/100;
+      var mapRGBA = ($(window).scrollTop()-470)/200;
       $('#nav_primary').css("background-color","rgba(0,0,0,"+mapRGBA+")");
       $('#nav_primary').css("padding-top",""+mapPaddingTop+"em");
     }
