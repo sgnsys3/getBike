dce3a3b59cfd47de9124f9e541768ddd80d19fdc
Pureewat Kaewpoy <pureewat_foa@hotmail.com>
2016-10-20 16:00:50 +0700

    new migrate config

---
+++ database/migrations/2016_10_20_085958_register_informations.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RegisterInformations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('register_informations', function (Blueprint $table) {
+        $table->increments('id');
+        $table->string('regis_code')->nullable();
+        $table->string('first_name');
+        $table->string('last_name');
+        $table->string('phone_number');
+        $table->string('email');
+        $table->date('birthday')->nullable();
+        $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('register_informations');
+    }
+}
